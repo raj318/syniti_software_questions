2eab641a04f0d7974a9f60dabb6c86fe343ba8f9
Pruthvi Raj Kudithi <pkudithi@unixdeva16.factset.com>
2021-04-17 07:19:06 -0400

    adding script to find invalid ids from data.json

---
+++ find_invalid_entries.py
@@ -0,0 +1,50 @@
+import hashlib
+import json
+
+items = ['name', 'address']
+
+def main():
+    """
+    1.load data as string from file(considering file is present in same path as script).
+    2.convert to json
+    3. to find invalid ids
+        i. each row, check for null name, address, zip
+        ii.if zip is present and valid.
+            * should be present
+            * should be either 5 digits
+            * should not be all zero's
+        iii. observed extra case from data.json that ID is duplicated.
+    """
+    data_file = './data.json'
+    data = []
+    with open(data_file) as fd:
+        data = json.load(fd)
+
+    invalids = []
+    cache = {}
+    for item in data:
+        print(item)
+        hshlib = hashlib.md5()
+        hshlib.update(str(item).encode('utf8'))
+        hash_id = hshlib.hexdigest()
+        if hash_id in cache:
+            invalids.append(item['id'])
+            continue
+        cache[hash_id] = 1
+        s = 0
+        for ele in items:
+            if not(ele in item and item[ele]):
+                print(f'{ele} is not valid for {item["id"]}')
+                invalids.append(item['id'])
+                s = 1
+                break
+        if s:
+            continue
+        if not('zip' in item and item['zip'] and len(item['zip']) == 5 \
+                and item['zip'].isdecimal() and item['zip'] != '00000'):
+            invalids.append(item['id'])
+    for item in invalids:
+        print(item)
+
+if __name__ == "__main__":
+    main()
